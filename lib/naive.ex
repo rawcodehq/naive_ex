fa95a90d7411f3a1464b3510625c7375a0328eca
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-03-10 11:58:04 +0530

    added map

--- lib/naive.ex
+++ lib/naive.ex
@@ -1,9 +1,14 @@
 defmodule Naive do
   defmodule Enum do
-    # Lists
-    def reduce([], acc, _fun) do
-      acc
+
+    def map(enumerable, map_fun) do
+      reduce(enumerable, [], fn x, acc ->
+        [map_fun.(x) | acc]
+      end) |> Elixir.Enum.reverse
     end
+
+    # Lists
+    def reduce([], acc, _fun), do: acc
     def reduce([x | rest], acc, fun) do
       acc = fun.(x, acc)
       reduce(rest, acc, fun)
--- test/naive_test.exs
+++ test/naive_test.exs
@@ -3,6 +3,18 @@ defmodule NaiveTest do
 
   import Naive.Enum
 
+  describe "Enum.map" do
+    test "should allow addition on elements" do
+      a = Enum.to_list(1..10)
+      assert Naive.Enum.map(a, fn x -> x + 1 end) == [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    end
+
+    test "should be able to upcase" do
+      assert map(["khaja", "mujju", "zainab"], &String.upcase/1) == ["KHAJA", "MUJJU", "ZAINAB"]
+    end
+
+  end
+
   describe "Enum.reduce" do
     test "returns acc for empty lists" do
       assert reduce([], 1, fn x, acc -> x + acc end) == 1
@@ -16,9 +28,15 @@ defmodule NaiveTest do
       assert reduce([5, 9], 3, fn x, acc -> x + acc end) == 17
     end
 
-    test "can add using reduce with a multiple elements" do
+    test "can add using reduce with multiple elements" do
       a = 1..10 |> Enum.to_list
       assert reduce(a, 0, fn x, acc -> x + acc end) == 55
     end
+
+    test "can multiply using reduce with multiple elements" do
+      a = 1..10 |> Enum.to_list
+      assert reduce(a, 1, fn x, acc -> x * acc end) == 3628800
+    end
   end
+
 end
